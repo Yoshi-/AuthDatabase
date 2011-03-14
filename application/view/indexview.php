fec2d81f2d3abde2e8717b4b6892c68cced34157
Yoshi- <b.arne@gmx.de>
2011-03-14 12:21:44 -0700

    Edited application/view/indexview.php via GitHub

--- application/view/indexview.php
+++ application/view/indexview.php
@@ -22,21 +22,71 @@ class indexView {
 			} else {
 					$pageLink .='<a href="index.php?'.http_build_query($data).'">' . $i . '</a> ';
 			}
-			if($i == 6) {
-				$pageLink .== ' ... ';
-				$i = $page_number - 5;
-			}
 			$i++;
 		}		
 		$template = new Template('templates', 'filter.tpl');
 		$template -> addVariable('names', $names)
 				  -> _loadTemplate();
+<?php
+class indexView {
+	public function showAuths($auths = array(), $names = array(), $page_number = 1) {
+		if(isset($_GET['page'])) $page = (int) $_GET['page'];
+		else $page = 1;
+		
+		if($page < 1) $page = 1;
+
+		$data = $_GET;
+		if(isset($data['page'])) unset($data['page']);
+		if(isset($data['pagenumber'])) unset($data['pagenumber']);
+
+		$pageLink = '<form action="index.php?'.http_build_query($data).'" method="post">';
+
+	
+		$i = 1;
+		while($i <= $page_number) {
+			$data['page'] = $i;
+
+			$pageLink .= '';
+
+			if($i == $page) {
+				$pageLink .='<b>' . $i .'</b> ';
+			} else {
+					$pageLink .= '<option value="'.$i.'">Page ' . $i . '</option> ';
+			}
+			$i++;
+		}		
+
+		$pageLink .= '</form>';
+
+		$template = new Template('templates', 'filter.tpl');
+		$template -> addVariable('names', $names)
+				  -> _loadTemplate();
+				  -> addVariable('page', $pageLink)
 		$filter = $template -> _run();
 			
 		$template = new Template('templates', 'showAuths.tpl');
 		$template -> addVariable('Auths', $auths)
 				  -> addVariable('filter', $filter)
 				  -> addVariable('page', $pageLink)
+				  -> _loadTemplate();
+		$content = $template -> _run();
+		return $content;
+	}
+	
+	public function showCheck() {
+		$template = new Template('templates', 'checkAuths.tpl');
+		$template -> _loadTemplate();
+		
+		$content = $template -> _run();
+		
+		return $content;
+	}
+} 
+		$filter = $template -> _run();
+			
+		$template = new Template('templates', 'showAuths.tpl');
+		$template -> addVariable('Auths', $auths)
+				  -> addVariable('filter', $filter)
 				  -> _loadTemplate();
 		$content = $template -> _run();
 		return $content;
