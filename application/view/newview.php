46bb272857539a72005d685a30b97dc5889e89bd
Yoshi <b.arne@gmx.de>
2011-05-15 03:35:40 -0700

    Edited application/view/newview.php via GitHub

--- application/view/newview.php
+++ application/view/newview.php
@@ -1,1 +1,20 @@
-<?phpclass newView {	private $template;		public function newAuths() {		$template = new Template('templates', 'addAuths.tpl');		$token = $_SESSION['token'];		$template -> addVariable('token', $token)				  -> _loadTemplate();				  		$content = $template -> _run();				return $content;	}		public function savedAuths($auths) {		return $auths;	}}
+<?php
+class newView {
+
+	private $template;
+	
+	public function newAuths() {
+		$template = new Template('templates', 'addAuths.tpl');
+		$token = $_SESSION['token'];
+		$template -> addVariable('token', $token)
+				  -> _loadTemplate();
+				  
+		$content = $template -> _run();
+		
+		return $content;
+	}
+	
+	public function savedAuths($auths) {
+		return $auths;
+	}
+}
